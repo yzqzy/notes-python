10f22b10534c70a3c02d24786ff04318f42a04d7
heora <yueluo.yang@gmail.com>
2024-01-06 21:26:05 +0800

    docs: 强制指定传参

--- base/05_class_04.py
+++ base/05_class_04.py
@@ -45,3 +45,50 @@ father.info()  # name heora, gender 男
 
 line()
 # ---------------------
+
+"""
+类方法
+
+classmethod cls 指向类对象，如果改动 class 名称，仍然可以使用 cls 获取类属性。
+"""
+
+
+class A:
+  name = 'cls_a'
+
+  @staticmethod
+  def get_attr():
+    print(A.name)
+
+  @classmethod
+  def get_attr_1(cls):
+    print(cls.name)
+
+
+a = A()
+a.get_attr()  # cls_a
+a.get_attr_1()  # cls_a
+
+line()
+# ---------------------
+
+"""
+* 强制指定传参
+"""
+
+
+class Person:
+  def __init__(self, *, name, age) -> None:
+    self.name = name
+    self.age = age
+
+  def info(self):
+    print(self.name, self.age)
+
+
+# person = Person(1, 2) # Person.__init__() takes 1 positional argument but 3 were given
+person = Person(name='heora', age=25)
+person.info()  # heora 25
+
+line()
+# ---------------------
