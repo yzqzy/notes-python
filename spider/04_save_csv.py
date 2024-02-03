0968ca44f1387e15e120f77601ef2b6715677da3
heora <yueluo.yang@gmail.com>
2024-02-03 12:08:09 +0800

    docs: update

--- spider/04_save_csv.py
+++ spider/04_save_csv.py
@@ -7,3 +7,29 @@ CSV, 全称 Comma Separated Values, 即逗号分隔值文件, 其文件以纯文
 """
 
 # # -----------------------------------
+
+"""
+数组写入
+"""
+
+# with open('spider/data_array_test.csv', 'w') as f:
+#   writer = csv.writer(f, delimiter='\t')
+#   writer.writerow(['name', 'age', 'gender'])
+#   writer.writerow(['Alice', 25, 'female'])
+#   writer.writerow(['Bob', 30, 'male'])
+#   writer.writerow(['Charlie', 35, 'female'])
+
+# # -----------------------------------
+
+"""
+字典写入
+"""
+
+with open('spider/data_dict_test.csv', 'w') as f:
+  writer = csv.DictWriter(f, fieldnames=['name', 'age', 'gender'])
+  writer.writeheader()
+  writer.writerow({'name': 'Alice', 'age': 25, 'gender': 'female'})
+  writer.writerow({'name': 'Bob', 'age': 30, 'gender': 'male'})
+  writer.writerow({'name': 'Charlie', 'age': 35, 'gender': 'female'})
+
+# # -----------------------------------
